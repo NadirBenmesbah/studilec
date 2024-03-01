c2d7b5816834f1106beddf2f8b3d2e57f7b3f395
Nadir Benmesbah <86564792+NadirBenmesbah@users.noreply.github.com>
2024-03-01 14:26:07 +0100

    Add files via upload

---
+++ error.js
@@ -0,0 +1,25 @@
+const navButton = document.getElementById('navButton');
+const navLinks = document.getElementById('navLinks');
+navButton.addEventListener('click', function () {
+    navLinks.classList.toggle('active');
+})
+const up = document.getElementById('up');
+window.addEventListener('scroll', () => {
+    if (window.scrollY === 0) {
+        up.classList.remove('activeScroll');
+    }
+    else {
+        up.classList.add('activeScroll');
+    }
+})
+up.addEventListener('click', function() {
+    window.scrollTo({
+      top: 0,
+    });
+  });
+const modulesButton = document.getElementById('modulesButton');
+const modules = document.getElementById('modules');
+modulesButton.addEventListener('click', function () {
+    modules.classList.toggle('modulesActive');
+    modulesButton.classList.toggle('modulesDivActive')
+})
