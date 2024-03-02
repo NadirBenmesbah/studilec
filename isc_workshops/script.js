806941b865d0be9166c6456b9dc40f0245b6fd13
Nadir Benmesbah <86564792+NadirBenmesbah@users.noreply.github.com>
2024-03-02 22:16:39 +0100

    Delete isc_workshops directory

--- isc_workshops/script.js
+++
@@ -1,7 +0,0 @@
-const navButton = document.getElementById('navButton');
-// console.log(navButton)
-const navLinks = document.getElementById('navLinks');
-// console.log(navLinks)
-navButton.addEventListener('click', function(){
-    navLinks.classList.toggle('active');
-})
